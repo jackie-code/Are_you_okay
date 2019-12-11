a3f76e516a29386ab90984b4054cb6d6b428cdc7
Jacqeuline Martin <paradiseprofessional88@gmail.com>
2019-12-11 11:35:46 -0500

    seperate api's in their own files

---
+++ cocktail.js
@@ -0,0 +1,81 @@
+/* *******************cocktail api**************** */
+//cocktail
+const apiKey = "1";
+const searchURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php"; 
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+//create params and connect base URL and params together
+function getDrinks(query) {
+  const params = {
+    api_key: apiKey,
+    s: query,
+  };
+  const queryString = formatQueryParams(params)
+  //connect the base URL to the queryString for the full ***url***
+  const url = searchURL + '?' + queryString;
+
+function displayResults(responseJson) {
+  //responseJson which is response.json() holds all data of object which was created in the fetch(url)
+  console.log(responseJson);
+  // if there are previous results, remove them
+  $('.cocktails').empty();
+  
+for (let i = 0; i < responseJson.drinks.length; i++){
+  const keys = Object.keys(responseJson.drinks[i])
+  console.log(keys);
+  
+  for (const key of keys) {
+  console.log(key)
+   if(key.includes('strIngredient') && responseJson.drinks[i][key] != null) {
+     //let str = keys.target();
+     console.log(responseJson.drinks[i][key])
+
+      //keys.slice(12, str.length)
+       $('.appendDrink').append(
+      `<p>${responseJson.drinks[i][key]}</p>`
+       )
+      }
+}
+
+    //add item to the results 
+    $('.appendDrink').append(
+      `<li>
+      <p>${responseJson.drinks[i].strDrink}</p>
+      <img src="${responseJson.drinks[i].strDrinkThumb}" width="235" height="235" alt="drink image">
+      <p>${responseJson.drinks[i].strInstructions}</p>
+      </li>`
+    )};
+};
+
+
+  //fetch to get respons.json() with all it's data
+  fetch(url) //get resonseJson data from url
+    .then(response => { //I promise when response is ready...
+    console.log(response)
+      if (response.ok) { // if response is true...
+        return response.json(); //return the json object
+      }
+      // statusText gives back the status error... example) OK for 200
+      throw new Error(response.statusText); //otherwise create an error
+    })
+    //response.json() becomes the variable responseJson here:
+    .then(responseJson =>   displayResults(responseJson))  //I promise when the responseJson is ready, to display the results to page
+    .catch(err => {  //if something messes up display this error:
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('#drink-form').submit(event => {
+    event.preventDefault();
+    const searchDrink = $('#drink-search').val();
+    getDrinks(searchDrink);
+  });
+}
+
+$(watchForm);
---
+++ holidays.js
@@ -0,0 +1,45 @@
+const api_key = "ee33e7e06cd36afdce7ff346e31834941f758ce2";
+const URL = "https://calendarific.com/api/v2/holidays?api_key=ee33e7e06cd36afdce7ff346e31834941f758ce2&country=US&year=2019";
+
+function fetchData(url) {
+  fetch(url)
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function (data) {
+      displayHolidays(data);
+    })
+    .catch(err => {
+      //if something messes up display this error:
+          $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+    }
+
+
+function displayHolidays(holidayResults){
+for (let i = 0; i < holidayResults.response.holidays.length; i++){
+   const date = holidayResults.response.holidays[i].date.iso;
+   const name = holidayResults.response.holidays[i].name;
+     $('.appendHoliday').append(
+      `<p>${date}, ${name}</p>`
+       )
+    }
+}
+
+
+//create a function that loops through all of holidayResults.response.holidays and create a new variable called current date and set it equal to newDateObject (look up the date api) then 
+
+
+
+
+
+
+
+function onHolidayButtonClick(event) {
+        event.preventDefault();
+        const holidayResults = fetchData(URL);
+        console.log("initial holiday results", holidayResults);
+        // displayHolidays();
+        
+      }
+$('.holidayClick').click(onHolidayButtonClick);
+
+

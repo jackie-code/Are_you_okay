fc7e369063b3538ac08b7409871e4b39a2770f5e
Jacqeuline Martin <paradiseprofessional88@gmail.com>
2019-12-27 22:24:40 -0500

    css fixes, button change

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 'use strict';
 
 
+// cool button
+// ***********************************************************
+
+$(window).ready(function(){
+  $(".boton").wrapInner('<div class=botontext></div>');
+      
+      $(".botontext").clone().appendTo( $(".boton") );
+      
+      $(".boton").append('<span class="twist"></span><span class="twist"></span><span class="twist"></span><span class="twist"></span>');
+      
+      $(".twist").css("width", "25%").css("width", "+=3px");
+  });
+
+
+
+
+// ***********************************************************
+
+
 //let levelpoints = `${getTotalPoints()}`;
 
 //listen for inputs
